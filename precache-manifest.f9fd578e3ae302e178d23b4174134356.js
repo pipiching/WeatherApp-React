1fc9edd6e1ee109352c62d417afd4f336e78cf35
Cliff_Li <cliff_li@migocorp.com>
2020-02-07 16:40:38 +0800

    Updates

---
+++ precache-manifest.f9fd578e3ae302e178d23b4174134356.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd58b25b11292885f0400c281477193",
+    "url": "/Weather-React/index.html"
+  },
+  {
+    "revision": "4ad88eec4efba946db2d",
+    "url": "/Weather-React/static/css/main.233646b9.chunk.css"
+  },
+  {
+    "revision": "1311594275810c4b7bdf",
+    "url": "/Weather-React/static/js/2.86f17c1b.chunk.js"
+  },
+  {
+    "revision": "5d046a911449cb2aa2829802bbc06aa0",
+    "url": "/Weather-React/static/js/2.86f17c1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ad88eec4efba946db2d",
+    "url": "/Weather-React/static/js/main.49a4f0b9.chunk.js"
+  },
+  {
+    "revision": "6ef6c69823ba53c60593",
+    "url": "/Weather-React/static/js/runtime-main.67b0840c.js"
+  },
+  {
+    "revision": "7ca9fe14f0863a8639463718f68ee69f",
+    "url": "/Weather-React/static/media/cog.7ca9fe14.svg"
+  },
+  {
+    "revision": "19e2ef132632c2a41a547fd987ea4332",
+    "url": "/Weather-React/static/media/day-clear.19e2ef13.svg"
+  },
+  {
+    "revision": "296112b168b03996ac80e9c7d324bd80",
+    "url": "/Weather-React/static/media/day-cloudy-fog.296112b1.svg"
+  },
+  {
+    "revision": "5c66b1345ba4954b591a9abd473fec8e",
+    "url": "/Weather-React/static/media/day-cloudy.5c66b134.svg"
+  },
+  {
+    "revision": "4b1ec37a80fcfeb3339628e1d941bd95",
+    "url": "/Weather-React/static/media/day-fog.4b1ec37a.svg"
+  },
+  {
+    "revision": "51c7cd390b88854769659caf903cf233",
+    "url": "/Weather-React/static/media/day-partially-clear-with-rain.51c7cd39.svg"
+  },
+  {
+    "revision": "dd8509241b9184ffc6498e27e256d12d",
+    "url": "/Weather-React/static/media/day-snowing.dd850924.svg"
+  },
+  {
+    "revision": "32a450a8eb3d385943af76f3738204b1",
+    "url": "/Weather-React/static/media/day-thunderstorm.32a450a8.svg"
+  },
+  {
+    "revision": "100517bb041464bfeb98befb5575ba9a",
+    "url": "/Weather-React/static/media/humid.100517bb.svg"
+  },
+  {
+    "revision": "bffa65e7f30ad0a23468893a58cf1445",
+    "url": "/Weather-React/static/media/loading.bffa65e7.svg"
+  },
+  {
+    "revision": "a99547e9b50556379a96c369d2cd8a8c",
+    "url": "/Weather-React/static/media/night-clear.a99547e9.svg"
+  },
+  {
+    "revision": "c78ef76288a695f6d4d010fd4441b8b7",
+    "url": "/Weather-React/static/media/night-cloudy-fog.c78ef762.svg"
+  },
+  {
+    "revision": "3f25314886812bb140ce00c75ce38df7",
+    "url": "/Weather-React/static/media/night-cloudy.3f253148.svg"
+  },
+  {
+    "revision": "2477d0a9b2c5d67dc8b806ff6dc30d42",
+    "url": "/Weather-React/static/media/night-fog.2477d0a9.svg"
+  },
+  {
+    "revision": "3a37b82a0663f3627389043001c0c190",
+    "url": "/Weather-React/static/media/night-partially-clear-with-rain.3a37b82a.svg"
+  },
+  {
+    "revision": "19cb9f62d708528aa8145d69d7d1c713",
+    "url": "/Weather-React/static/media/night-snowing.19cb9f62.svg"
+  },
+  {
+    "revision": "5f007c776d1d327f40da690c8014a9d8",
+    "url": "/Weather-React/static/media/night-thunderstorm.5f007c77.svg"
+  },
+  {
+    "revision": "4c89b6a39d330a308f9920296a5e2f6b",
+    "url": "/Weather-React/static/media/redo.4c89b6a3.svg"
+  },
+  {
+    "revision": "458b2ff7545a986732f3a69dfe41799e",
+    "url": "/Weather-React/static/media/windspeed.458b2ff7.svg"
+  }
+]);
