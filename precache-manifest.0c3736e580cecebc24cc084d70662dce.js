e3876873473becd9805bafb70231748172450d8f
Cliff_Li <cliff_li@migocorp.com>
2020-02-07 16:52:08 +0800

    Updates

---
+++ precache-manifest.0c3736e580cecebc24cc084d70662dce.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f913252a585003411425525f83ba50",
+    "url": "/WeatherApp-React/index.html"
+  },
+  {
+    "revision": "e06b7e15df18c52a44da",
+    "url": "/WeatherApp-React/static/css/main.233646b9.chunk.css"
+  },
+  {
+    "revision": "a873ac45268db388b2eb",
+    "url": "/WeatherApp-React/static/js/2.222ced59.chunk.js"
+  },
+  {
+    "revision": "5d046a911449cb2aa2829802bbc06aa0",
+    "url": "/WeatherApp-React/static/js/2.222ced59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e06b7e15df18c52a44da",
+    "url": "/WeatherApp-React/static/js/main.366642ea.chunk.js"
+  },
+  {
+    "revision": "5e4dbba0b650480c8bf7",
+    "url": "/WeatherApp-React/static/js/runtime-main.99ef3c90.js"
+  },
+  {
+    "revision": "7ca9fe14f0863a8639463718f68ee69f",
+    "url": "/WeatherApp-React/static/media/cog.7ca9fe14.svg"
+  },
+  {
+    "revision": "19e2ef132632c2a41a547fd987ea4332",
+    "url": "/WeatherApp-React/static/media/day-clear.19e2ef13.svg"
+  },
+  {
+    "revision": "296112b168b03996ac80e9c7d324bd80",
+    "url": "/WeatherApp-React/static/media/day-cloudy-fog.296112b1.svg"
+  },
+  {
+    "revision": "5c66b1345ba4954b591a9abd473fec8e",
+    "url": "/WeatherApp-React/static/media/day-cloudy.5c66b134.svg"
+  },
+  {
+    "revision": "4b1ec37a80fcfeb3339628e1d941bd95",
+    "url": "/WeatherApp-React/static/media/day-fog.4b1ec37a.svg"
+  },
+  {
+    "revision": "51c7cd390b88854769659caf903cf233",
+    "url": "/WeatherApp-React/static/media/day-partially-clear-with-rain.51c7cd39.svg"
+  },
+  {
+    "revision": "dd8509241b9184ffc6498e27e256d12d",
+    "url": "/WeatherApp-React/static/media/day-snowing.dd850924.svg"
+  },
+  {
+    "revision": "32a450a8eb3d385943af76f3738204b1",
+    "url": "/WeatherApp-React/static/media/day-thunderstorm.32a450a8.svg"
+  },
+  {
+    "revision": "100517bb041464bfeb98befb5575ba9a",
+    "url": "/WeatherApp-React/static/media/humid.100517bb.svg"
+  },
+  {
+    "revision": "bffa65e7f30ad0a23468893a58cf1445",
+    "url": "/WeatherApp-React/static/media/loading.bffa65e7.svg"
+  },
+  {
+    "revision": "a99547e9b50556379a96c369d2cd8a8c",
+    "url": "/WeatherApp-React/static/media/night-clear.a99547e9.svg"
+  },
+  {
+    "revision": "c78ef76288a695f6d4d010fd4441b8b7",
+    "url": "/WeatherApp-React/static/media/night-cloudy-fog.c78ef762.svg"
+  },
+  {
+    "revision": "3f25314886812bb140ce00c75ce38df7",
+    "url": "/WeatherApp-React/static/media/night-cloudy.3f253148.svg"
+  },
+  {
+    "revision": "2477d0a9b2c5d67dc8b806ff6dc30d42",
+    "url": "/WeatherApp-React/static/media/night-fog.2477d0a9.svg"
+  },
+  {
+    "revision": "3a37b82a0663f3627389043001c0c190",
+    "url": "/WeatherApp-React/static/media/night-partially-clear-with-rain.3a37b82a.svg"
+  },
+  {
+    "revision": "19cb9f62d708528aa8145d69d7d1c713",
+    "url": "/WeatherApp-React/static/media/night-snowing.19cb9f62.svg"
+  },
+  {
+    "revision": "5f007c776d1d327f40da690c8014a9d8",
+    "url": "/WeatherApp-React/static/media/night-thunderstorm.5f007c77.svg"
+  },
+  {
+    "revision": "4c89b6a39d330a308f9920296a5e2f6b",
+    "url": "/WeatherApp-React/static/media/redo.4c89b6a3.svg"
+  },
+  {
+    "revision": "458b2ff7545a986732f3a69dfe41799e",
+    "url": "/WeatherApp-React/static/media/windspeed.458b2ff7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Weather-React/precache-manifest.f9fd578e3ae302e178d23b4174134356.js"
+  "/WeatherApp-React/precache-manifest.0c3736e580cecebc24cc084d70662dce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Weather-React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WeatherApp-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
